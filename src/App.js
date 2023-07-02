e561aa145f3d29aac0cafbdfb4812e6a240fa44f
Raghav Agrawal <raghav.agrawal@spinny.com>
2023-07-03 02:26:34 +0530

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import RemoveBg from "./RemoveBg";
-// import RemoveBGFree from "./RemoveBGFree";
 
 const App = () => {
 
   return <>
     <h1>Hello I am React</h1>
     <RemoveBg />
-    {/* <RemoveBGFree/> */}
   </>
 }
 export default App;
